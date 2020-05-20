6c0fe1e2e22135066303bb1772059d41a4e4f29d
Tamara Smiljanic <babictamara@yahoo.com>
2020-05-20 09:40:10 +0200

    Add project files.

---
+++ SeleniumTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Cas30SeleniumTests
+{
+    class SeleniumTests
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        string url = "";
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+}
